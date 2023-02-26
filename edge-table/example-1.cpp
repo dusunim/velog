c94229dd3a8479ab6c9947cd4a4d47980414d05f
Dusu Kim <dusu.kim@medit.com>
2023-02-26 13:48:45 +0900

    add edge-table/example-1.cpp

---
+++ edge-table/example-1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <set>
+
+struct Face
+{
+	int vertices[3];
+
+	int& operator[](size_t n) { return vertices[n]; }
+	int operator[](size_t n) const { return vertices[n]; }
+};
+
+struct Edge
+{
+	int vertices[2];
+
+	auto& lo() const noexcept { return vertices[vertices[0] >= vertices[1]]; }
+	auto& hi() const noexcept { return vertices[vertices[0] < vertices[1]]; }
+	auto oriented() const noexcept { return std::tie(lo(), hi()); }
+	bool operator<(const Edge& rhs) const noexcept { return oriented() < rhs.oriented(); }
+};
+
+int main()
+{
+	std::vector<Face> faces = {{ 4, 3, 2 }, { 1, 6, 0 }, { 3, 5, 6 }, { 4, 5, 3 }};
+	std::set<Edge> edges;
+
+	for (auto& face : faces)
+	{
+		edges.insert({ face[0], face[1] });
+		edges.insert({ face[1], face[2] });
+		edges.insert({ face[2], face[0] });
+	}
+	
+	for (auto& edge : edges)
+	{
+		printf("%d, %d\n", edge.lo(), edge.hi());
+	}
+
+	return 0;
+}
